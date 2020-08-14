fcb70611c2bcf6e4446411fd2b8b0ccbf6c83cfe
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-14 11:53:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(ray) {
+    let rayCopy = [ray[0], ray[1]];
+return(rayCopy)
+}
+
+const returnLastTwoDrivers = function(ray) {
+    let rayCopy = [ray[ray.length - 2], ray[ray.length - 1]]
+return(rayCopy);
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(a) {
+    return function(b){
+        return (a * b)
+    } 
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(selectingDrivers, something) {
+    return something(selectingDrivers)
+}
+
